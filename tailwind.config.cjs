95fe407379436fffdbceef9e5883670d957435fc
parables <38154990+Parables@users.noreply.github.com>
2022-09-12 21:52:43 +0000

    added svg support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ const config = {
     extend: {},
   },
 
-  plugins: [forms, daisyui],
+  plugins: [daisyui],
   daisyui: {
     themes: false, //["light", "dark", "cmyk"],
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import svgr from 'vite-plugin-svelte-svgr';
 import type { UserConfig } from 'vite';
 
+
+const svgoOptions = {
+	multipass: true,
+	plugins: [
+		// Ensuring viewbox isn't removed.
+		{
+			name: 'preset-default',
+			params: {
+				overrides: {
+					removeViewBox: false
+				}
+			}
+		},
+		{
+			// setting fill attribute to "currentColor"
+			name: 'addAttributesToSVGElement',
+			params: {
+				attributes: [{ fill: 'currentColor' }]
+			}
+		}
+	]
+};
+
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()
+,svgr({
+	svgo: svgoOptions
+})]
 };
 
 export default config;
