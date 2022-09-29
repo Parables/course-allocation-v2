dde5e4195a940d8ddcede68b11881131ee76ac64
parables <38154990+Parables@users.noreply.github.com>
2022-09-29 15:48:54 +0000

    initial implementation of image upload

--- src/routes/(app)/lecturers/new/+page.svelte
+++ src/routes/(app)/lecturers/new/+page.svelte
@@ -7,22 +7,23 @@
 	import deleteIcon from '$lib/assets/icons/trash.svg?raw';
 	import saveIcon from '$lib/assets/icons/save.svg?raw';
 	import type { ActionData } from './$types';
-	import { QualificationSchema } from '$lib/data/types/lecturer';
+	import { CreateLecturerSchema, QualificationSchema } from '$lib/data/types/lecturer';
 	import type { LecturerType } from '$lib/data/types/lecturer';
 	import { ValidationError } from 'myzod';
 	import EasyToast from '$lib/components/easy-toast.svelte';
 	import { assets } from '$app/paths';
-
-	export let form: ActionData;
-
-	$: console.log('form', form);
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	// export let form: ActionData;
+	let formData: Record<string, any> = {};
+	$: console.log('formData', formData);
 
 	let qualifications: LecturerType['qualifications'] = {};
 	let q: Record<string, any> = {};
 	let qCount = 0;
 	let qError: string | undefined = undefined;
-	let strQ: string;
-	$: strQ = JSON.stringify(qualifications);
+	// let strQ: string;
+	// $: strQ = JSON.stringify(qualifications);
 	$: qCount = Object.keys(qualifications).length;
 
 	const saveQualification = () => {
@@ -33,6 +34,7 @@
 		qError = undefined;
 
 		qualifications = { ...qualifications, [qCount]: validated };
+		formData.qualifications = { ...qualifications };
 		console.log(qualifications);
 	};
 
@@ -44,7 +46,7 @@
 
 	let files: FileList | null | undefined = undefined;
 
-	let profilePicture: any = null;
+	// let profilePicture: any = null;
 
 	$: {
 		if (files) {
@@ -52,10 +54,58 @@
 			let reader = new FileReader();
 			reader.readAsDataURL(image);
 			reader.onload = (e) => {
-				profilePicture = e?.target?.result ?? null;
-				console.log(profilePicture);
+				formData.profilePicture = e?.target?.result ?? null;
+				uploadProfilePicture('cbquvjwxnuci');
 			};
 		}
+	}
+
+	const handleSubmit = async () => {
+		const { staffID, fullName, gender, email, phoneNumber, qualifications } = formData;
+		const validated = CreateLecturerSchema.try({
+			staffID,
+			fullName,
+			gender,
+			email,
+			phoneNumber,
+			qualifications: JSON.parse(qualifications)
+		});
+
+		console.log(validated);
+		if (validated instanceof ValidationError) {
+			return (formData.error = { message: validated.message });
+		}
+
+		const response = await fetch(`${$page.url.origin}/api/lecturers`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify(validated)
+		});
+
+		const result = await response.json();
+
+		if (result.key) {
+			uploadProfilePicture(result.key);
+			goto('/lecturers');
+		}
+	};
+
+	async function uploadProfilePicture(key: string) {
+		const data: Record<string, any> = {};
+		const imgData = formData.profilePicture.split(',');
+		data.profilePicture = imgData[1];
+		console.log(data);
+		await fetch(`${$page.url.origin}/api/lecturers/${key}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
 	}
 	/* 	const onFileSelected = (e) => {
 		let image = e.target.files[0];
@@ -83,199 +133,202 @@
 		<hr class="mt-6" />
 
 		<!-- add lecturer form -->
-		<form method="POST">
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-32 mt-[85px] w-full px-10">
-				<!-- Personal details -->
-				<div class="flex flex-col gap-y-5 w-full">
-					<div>
-						<h2 class="text-sm font-poppins font-semibold">Personal Details</h2>
-						<h5 class="text-xs font-poppins font-light ">
-							Fill in personal details of the lecturer
-						</h5>
-					</div>
+		<!-- <form method="POST"> -->
+		<div class="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-32 mt-[85px] w-full px-10">
+			<!-- Personal details -->
+			<div class="flex flex-col gap-y-5 w-full">
+				<div>
+					<h2 class="text-sm font-poppins font-semibold">Personal Details</h2>
+					<h5 class="text-xs font-poppins font-light ">Fill in personal details of the lecturer</h5>
+				</div>
 
-					<!-- TODO: Upoad images of lecturer -->
+				<!-- TODO: Upoad images of lecturer -->
 
-					<label for="profilePicturePicker" class="cursor-pointer">
-						<img
-							class="w-36 h-36 ring-4 ring-purple-300 ring-offset-4 object-cover rounded-md"
-							src={profilePicture ?? `${assets}/img-placeholder.png`}
-							alt="Extra large avatar"
-						/>
-						<input
-							type="file"
-							name="profilePicturePicker"
-							id="profilePicturePicker"
-							class="sr-only"
-							bind:files
-						/>
-						<input
+				<label for="profilePicturePicker" class="cursor-pointer">
+					<img
+						class="w-36 h-36 ring-4 ring-purple-300 ring-offset-4 object-cover rounded-md"
+						src={formData.profilePicture ?? `${assets}/img-placeholder.png`}
+						alt="Lecturer's Profile Pic"
+					/>
+					<input
+						type="file"
+						name="profilePicturePicker"
+						id="profilePicturePicker"
+						class="sr-only"
+						bind:files
+					/>
+					<!-- <input
 							type="file"
 							name="profilePicture"
 							id="profilePicture"
 							class="sr-only"
 							bind:value={profilePicture}
-						/>
-					</label>
+						/> -->
+				</label>
 
-					<InputField
-						id="staffID"
-						name="staffID"
-						label="Staff ID"
-						required
-						initial={form?.staffID ?? ''}
-					/>
-					<InputField
-						id="fullName"
-						name="fullName"
-						label="Full Name"
-						autocomplete="name"
-						required
-						initial={form?.fullName ?? ''}
-					/>
-					<select
-						id="gender"
-						name="gender"
-						label="Gender"
-						list="gender-options"
-						required
-						value={form?.gender ?? ''}
-						class="select select-bordered "
-					>
-						<option value="" disabled>Select Gender</option>
-						<option value="male">Male</option>
-						<option value="female">Female</option>
-					</select>
+				<InputField
+					id="staffID"
+					name="staffID"
+					label="Staff ID"
+					required
+					initial={formData?.staffID ?? ''}
+					bind:value={formData.staffID}
+				/>
+				<InputField
+					id="fullName"
+					name="fullName"
+					label="Full Name"
+					autocomplete="name"
+					required
+					initial={formData?.fullName ?? ''}
+					bind:value={formData.fullName}
+				/>
+
+				<select
+					id="gender"
+					name="gender"
+					label="Gender"
+					list="gender-options"
+					required
+					bind:value={formData.gender}
+					class="select select-bordered "
+				>
+					<option value="" disabled>Select Gender</option>
+					<option value="male">Male</option>
+					<option value="female">Female</option>
+				</select>
+			</div>
+
+			<!-- Contanct details -->
+			<div class="flex flex-col gap-y-5 w-full">
+				<div>
+					<h2 class="text-sm font-poppins font-semibold">Contact Details</h2>
+					<h5 class="text-xs font-poppins font-light">Fill in contact details of the lecturer</h5>
 				</div>
-
-				<!-- Contanct details -->
-				<div class="flex flex-col gap-y-5 w-full">
-					<div>
-						<h2 class="text-sm font-poppins font-semibold">Contact Details</h2>
-						<h5 class="text-xs font-poppins font-light">Fill in contact details of the lecturer</h5>
-					</div>
-					<InputField
-						id="email"
-						name="email"
-						label="Email"
-						type="email"
-						autocomplete="email"
-						required
-						initial={form?.email ?? ''}
-					/>
-					<InputField
-						id="phoneNumber"
-						name="phoneNumber"
-						label="Phone Number"
-						autocomplete="tel"
-						required
-						initial={form?.phoneNumber ?? ''}
-					/>
+				<InputField
+					id="email"
+					name="email"
+					label="Email"
+					type="email"
+					autocomplete="email"
+					required
+					initial={formData?.email ?? ''}
+					bind:value={formData.email}
+				/>
+				<InputField
+					id="phoneNumber"
+					name="phoneNumber"
+					label="Phone Number"
+					autocomplete="tel"
+					required
+					initial={formData?.phoneNumber ?? ''}
+					bind:value={formData.phoneNumber}
+				/>
+			</div>
+			<!-- Qualification details -->
+			<div class="flex flex-col  gap-y-5 w-full">
+				<div>
+					<h2 class="text-sm font-poppins font-semibold">Qualification Details</h2>
+					<h5 class="text-xs font-poppins font-light">
+						Fill in qualification details of the lecturer. All fields marked * are required
+					</h5>
 				</div>
-				<!-- Qualification details -->
-				<div class="flex flex-col  gap-y-5 w-full">
-					<div>
-						<h2 class="text-sm font-poppins font-semibold">Qualification Details</h2>
-						<h5 class="text-xs font-poppins font-light">
-							Fill in qualification details of the lecturer. All fields marked * are required
-						</h5>
-					</div>
-					<input name="qualifications" id="qualifications *" class="sr-only" bind:value={strQ} />
+				<!-- <input name="qualifications" id="qualifications *" class="sr-only" bind:value={strQ} /> -->
 
-					<div class="flex w-full items-center gap-5">
-						<div class="dropdown dropdown-end">
-							<!-- <label tabindex="0" class="btn btn-ghost rounded-btn">Dropdown</label> -->
-							<span tabindex="0" class="btn btn-outline gap-x-5">
-								<p>{qCount} Qualifications Added</p>
-								{@html viewIcon}</span
-							>
+				<div class="flex w-full items-center gap-5">
+					<div class="dropdown dropdown-end">
+						<!-- <label tabindex="0" class="btn btn-ghost rounded-btn">Dropdown</label> -->
+						<span tabindex="0" class="btn btn-outline gap-x-5">
+							<p>{qCount} Qualifications Added</p>
+							{@html viewIcon}</span
+						>
 
-							<ul
-								tabindex="0"
-								class="mt-4 rounded-md shadow-md menu dropdown-content bg-base-100 w-96 h-80 overflow-y-auto p-5"
-							>
-								<li class="text-sm">Click on an item to remove it</li>
-								{#each Object.entries(qualifications) as [k, v], i (k)}
-									<li class="hover-bordered my-4" on:click={() => deleteQ(k)}>
-										<span class="grid  grid-cols-12 border border-slate-500 rounded pr-4">
-											<span class="grid grid-cols-1 text-sm col-span-11">
-												<p class="text-left">University: {v.university}</p>
-												<p class="text-left">Programme: {v.programme}</p>
-												<p class="text-left">Certificate: {v.certificate}</p>
-												<p class="text-left">Period: {v.startDate} to {v.endDate}</p>
-											</span>
-											<span>{@html deleteIcon}</span>
+						<ul
+							tabindex="0"
+							class="mt-4 rounded-md shadow-md menu dropdown-content bg-base-100 w-96 h-80 overflow-y-auto p-5"
+						>
+							<li class="text-sm">Click on an item to remove it</li>
+							{#each Object.entries(qualifications) as [k, v], i (k)}
+								<li class="hover-bordered my-4" on:click={() => deleteQ(k)}>
+									<span class="grid  grid-cols-12 border border-slate-500 rounded pr-4">
+										<span class="grid grid-cols-1 text-sm col-span-11">
+											<p class="text-left">University: {v.university}</p>
+											<p class="text-left">Programme: {v.programme}</p>
+											<p class="text-left">Certificate: {v.certificate}</p>
+											<p class="text-left">Period: {v.startDate} to {v.endDate}</p>
 										</span>
-									</li>
-								{/each}
-							</ul>
-						</div>
+										<span>{@html deleteIcon}</span>
+									</span>
+								</li>
+							{/each}
+						</ul>
 					</div>
+				</div>
 
-					{#if qError}
-						<p class="text-sm text-red-500 font-semibold">{qError}</p>
-					{/if}
+				{#if qError}
+					<p class="text-sm text-red-500 font-semibold">{qError}</p>
+				{/if}
+				<InputField
+					id="university"
+					name="university"
+					label="University *"
+					required
+					bind:value={q.university}
+				/>
+				<InputField
+					id="programme"
+					name="programme"
+					label="Programme *"
+					required
+					bind:value={q.programme}
+				/>
+				<InputField
+					id="certificate"
+					name="certificate"
+					label="Certificate *"
+					required
+					bind:value={q.certificate}
+				/>
+				<div class="grid w-full grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-y-0 md:gap-x-5 gap-x-0">
 					<InputField
-						id="university"
-						name="university"
-						label="University *"
+						id="startDate"
+						name="startDate"
+						label="Start Date *"
+						type="date"
 						required
-						bind:value={q.university}
+						bind:value={q.startDate}
 					/>
 					<InputField
-						id="programme"
-						name="programme"
-						label="Programme *"
+						id="endDate"
+						name="endDate"
+						label="End Date *"
+						type="date"
 						required
-						bind:value={q.programme}
+						bind:value={q.endDate}
 					/>
-					<InputField
-						id="certificate"
-						name="certificate"
-						label="Certificate *"
-						required
-						bind:value={q.certificate}
-					/>
-					<div class="grid w-full grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-y-0 md:gap-x-5 gap-x-0">
-						<InputField
-							id="startDate"
-							name="startDate"
-							label="Start Date *"
-							type="date"
-							required
-							bind:value={q.startDate}
-						/>
-						<InputField
-							id="endDate"
-							name="endDate"
-							label="End Date *"
-							type="date"
-							required
-							bind:value={q.endDate}
-						/>
-					</div>
-					<div
-						class="btn btn-outline w-full capitalize gap-4 inline-flex"
-						on:click={saveQualification}
-					>
-						{@html saveIcon} Save Qualifications
-					</div>
+				</div>
+				<div
+					class="btn btn-outline w-full capitalize gap-4 inline-flex"
+					on:click={saveQualification}
+				>
+					{@html saveIcon} Save Qualifications
 				</div>
 			</div>
-			<EasyToast
-				show={form?.error?.message}
-				message={form?.error?.message ?? 'Something went wrong'}
-				type="error"
-			/>
+		</div>
+		<!-- <EasyToast
+			show={formData?.error?.message}
+			message={form?.error?.message ?? 'Something went wrong'}
+			type="error"
+		/>
 
-			<EasyToast
-				show={form?.success?.message}
-				message={form?.success?.message ?? 'Updated successfully	'}
-				type="success"
-			/>
-			<!-- bottom divider -->
-			<hr class="my-10" />
-			<Button classNames=" md:w-auto md:ml-10">Submit</Button>
-		</form>
+		<EasyToast
+			show={form?.success?.message}
+			message={form?.success?.message ?? 'Updated successfully	'}
+			type="success"
+		/> -->
+		<!-- bottom divider -->
+		<hr class="my-10" />
+		<Button classNames=" md:w-auto md:ml-10">Submit</Button>
+		<!-- </form> -->
 	</div>
 </div>
--- src/routes/api/lecturers/[id]/+server.ts
+++ src/routes/api/lecturers/[id]/+server.ts
@@ -2,15 +2,33 @@ import type { RequestHandler } from './$types';
 import { Deta } from 'deta'; // import Deta
 import { error, json } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import { writeFileSync } from 'fs';
 
 // Initialize with a Project Key
 const deta = Deta(env.DETA_PROJECT_KEY);
 
 // This how to connect to or create a database.
 const db = deta.Base('lecturers');
+const drive = deta.Drive('photos');
 
 export const GET: RequestHandler = async ({ params }) => {
 	return json(await db.get(params.id));
+};
+
+// endpoint to handle uploads
+export const POST: RequestHandler = async ({ request, params }) => {
+	try {
+		const { profilePicture: data } = await request.json();
+		console.log('image recieved', data);
+
+		writeFileSync(`static/avatar.png`, data, 'base64');
+
+		const buffer = Buffer.from(data, 'base64');
+		drive.put(`lecturers/${params.id}.png`, { data: buffer, contentType: 'image/png' });
+	} catch (err) {
+		throw error(400, `Image upload failed... ${err}`);
+	}
+	return json({ message: 'upload was successful' });
 };
 
 export const PATCH: RequestHandler = async ({ request, params }) => {
