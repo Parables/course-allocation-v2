9174f3a611f3ae638e9e7208c955d2c4b823dba9
parables <38154990+Parables@users.noreply.github.com>
2022-10-24 10:00:55 +0000

    last checkpoint

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-	import EasyToast from '$lib/components/easy-toast.svelte';
 	import Button from '$lib/components/button.svelte';
-	import Grid from 'gridjs-svelte';
-	import type { UserConfig } from 'gridjs';
 
 	import { page } from '$app/stores';
-	import { enhance } from '$app/forms';
 	import backIcon from '$lib/assets/icons/chevron-left.svg?raw';
-	import viewIcon from '$lib/assets/icons/eye.svg?raw';
 	import editIcon from '$lib/assets/icons/edit.svg?raw';
 	import printIcon from '$lib/assets/icons/printer.svg?raw';
 	import type { PageData } from './$types';
-	import type { ServerStorageOptions } from 'gridjs/dist/src/storage/server';
-	import type { CourseType } from '$lib/data/types/course';
-	import { suffixWith } from '$lib/utils';
-	import { escape } from 'svelte/internal';
-
-	import { getUser } from 'lucia-sveltekit/client';
-
-	const user = getUser();
 
 	export let data: PageData;
 
@@ -28,24 +15,10 @@
 	$: {
 		({ lecturer, assignedCourses } = data);
 	}
-
-	let printEl: HTMLDivElement | undefined;
-
-	// const handlePrint = () => {
-	// 	// html2pdf(printEl);
-	// };
 </script>
 
-<svelte:head>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
-		integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
-		crossorigin="anonymous"
-		referrerpolicy="no-referrer"
-	></script>
-</svelte:head>
 <div class="w-full h-full flex flex-col">
-	<div class="flex w-full items-center justify-between">
+	<div class="flex w-full items-center justify-between print:hidden">
 		<div class="flex items-center w-full ">
 			<a href="/" class=" rounded border-purple-500 text-purple-500 border mr-4">
 				{@html backIcon}
@@ -53,18 +26,22 @@
 			<h1 class="font-poppins text-2xl font-bold">Lecturer's Profile</h1>
 		</div>
 		<!-- print button -->
-		<a href="{$page.url.toString()}/print">
-			<Button classNames="w-auto inline-flex items-center gap-x-2">{@html printIcon} Print</Button>
-		</a>
+		<!-- <a href="{$page.url.toString()}/print"> -->
+		<Button on:click={() => window.print()} classNames="w-auto inline-flex items-center gap-x-2"
+			>{@html printIcon} Print</Button
+		>
+		<!-- </a> -->
 	</div>
 
-	<div id="html2pdf" bind:this={printEl} class="overflow-y-hidden flex-1 mb-10">
+	<div class="overflow-y-hidden flex-1 mb-10">
 		<div
-			class="grid grid-cols-1 md:grid-cols-3 gap-y-10 gap-x-2  h-full w-full py-10 overflow-y-auto md:overflow-hidden"
+			class="grid grid-cols-1 md:grid-cols-3 print:grid-cols-1 gap-y-10 gap-x-2  h-full w-full py-10 overflow-y-auto md:overflow-hidden print:overflow-hidden"
 		>
 			<!-- Profile -->
 
-			<ul class="grid grid-cols-1 gap-y-5 w-full  gap-x-4 px-6 md:overflow-y-auto">
+			<ul
+				class="grid grid-cols-1 gap-y-5 w-full  gap-x-4 px-6 md:overflow-y-auto print:overflow-hidden"
+			>
 				<li>
 					<h3 class="font-semibold text-base text-gray-400 uppercase">Lecturers Profile</h3>
 				</li>
@@ -102,7 +79,7 @@
 					<p class="text-base mb-4">{lecturer.phoneNumber}</p>
 				</li>
 
-				<li>
+				<li class="print:hidden">
 					<a href="/lecturers/edit/{lecturer.key}">
 						<Button classNames="w-8/12">Edit Profile</Button>
 					</a>
@@ -115,7 +92,7 @@
 				<div>
 					<a
 						href="/schedules?lecturer={lecturer.key}"
-						class="py-4 text-base font-semibold text-center uppercase flex w-full items-center gap-x-5 link link-hover"
+						class="print:hidden py-4 text-base font-semibold text-center uppercase flex w-full items-center gap-x-5 link link-hover"
 						>Assigned Courses {@html editIcon}</a
 					>
 
